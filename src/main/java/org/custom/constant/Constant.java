49e8a227a8a9fd8eea54ca38e187a65b2d981393
simson-m <simson.m@mind-graph.com>
2024-05-31 09:41:35 +0530

    feat: added constant:

---
+++ src/main/java/org/custom/constant/Constant.java
@@ -0,0 +1,10 @@
+package org.custom.constant;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Constant {
+    public static final String HELLO = "Hello from dependency!";
+    public static final List<String> PROGRAM_LANG = Arrays.asList("Java","C","GO");
+    public static final List<String> FRAME_WORK = Arrays.asList("SPRING BOOT","GO LANG","VAADIN");
+}
